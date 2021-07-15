b87f87fbeef42641342fb20ef8de9be1d0db368f
Ben Laycock <benlaycock17@gmail.com>
2021-07-15 20:58:55 +0100

    Custom Scrollbar / Placeholder Images

--- ModernWebsite/JAVASCRIPT/ScrollFunction.js
+++ ModernWebsite/JAVASCRIPT/ScrollFunction.js
@@ -2,10 +2,16 @@ $( document ).ready(function() {
     $("body").scrollTop(0);
     var numberOfSections = document.getElementsByClassName("section").length;
 
+    let progress = document.getElementById("progressBar");
+
     $(window).scroll(function() {
         var maxScrollTop = $("#container").height();
         var maxForAllSections = maxScrollTop*numberOfSections;
         $("body").css("height", maxForAllSections);
+
+        var test = maxScrollTop*(numberOfSections-1);
+        let progressWidth = (window.pageYOffset / test) * 100;
+        progress.style.width = progressWidth + "%";
 
         var scroll = $("body").scrollTop();
         var maxHeight = $(document).height();
