7b5ab027865075fb8290651ef4a28ce6c07a3242
Ben Laycock <benlaycock17@gmail.com>
2021-07-16 18:19:50 +0100

    Phone Compatibility

--- ModernWebsite/JAVASCRIPT/ScrollFunction.js
+++ ModernWebsite/JAVASCRIPT/ScrollFunction.js
@@ -4,12 +4,24 @@ $( document ).ready(function() {
 
     let progress = document.getElementById("progressBar");
 
+    updateDisplay();
+    $(window).resize(function() {
+        updateDisplay();
+    });
+
     $(window).scroll(function() {
+        let sections = document.getElementsByClassName("section");
+        var totalHeight = 0;
+        for (let i = 0; i < sections.length-1; i++) {
+            totalHeight += sections[i].offsetHeight;
+        }
+        console.log(totalHeight);
+
         var maxScrollTop = $("#container").height();
         var maxForAllSections = maxScrollTop*numberOfSections;
         $("body").css("height", maxForAllSections);
 
-        var test = maxScrollTop*(numberOfSections-1);
+        var test = totalHeight;
         let progressWidth = (window.pageYOffset / test) * 100;
         progress.style.width = progressWidth + "%";
 
@@ -20,3 +32,21 @@ $( document ).ready(function() {
         $("#container").css("left", -value*numberOfSections + "%");
     });
 });
+
+function updateDisplay()
+{
+    let width = $("body").width();
+    $("#container").css("position", "fixed");
+    $("#container").css("display", "flex");
+
+    $(".flexContainer").css("flex-wrap", "wrap");
+    $(".flexContainer").css("flex-direction", "row");
+    if (width < 1000)
+    {
+        $("#container").css("position", "initial");
+        $("#container").css("display", "initial");
+
+        $(".flexContainer").css("flex-wrap", "no-wrap");
+        $(".flexContainer").css("flex-direction", "column");
+    }
+}
